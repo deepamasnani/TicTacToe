12cc7a9d641481a0793debe0a32ce506fd6afe17
deepamasnani <asnani.deepam@gmail.com>
2021-07-23 00:42:41 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from './components/Board';
+
+const App = () => {
+  return (
+    <>
+      <h1>TIC TAC TOE!</h1>
+      <Board />
+      
+    </>
+  );
+};
+
+export default App;

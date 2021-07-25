06a85134d30745eef46bd81d5f81f10bf462823a
deepamasnani <asnani.deepam@gmail.com>
2021-07-26 02:16:37 +0530

    added history navigator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from "./helpers";
 
 import './styles/root.scss'
@@ -15,6 +16,7 @@ const App = () => {
   const current = history[currentMove];
 
   const winner = calculateWinner(current.board);
+  
 
   const message = winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : 'O'}`
 
@@ -39,13 +41,16 @@ const App = () => {
       } )
       setCurrentMove(prev => prev+1);
   }
+  const moveTo = (move) => {
+    setCurrentMove(move);
+  }
 
   return (
     <div className = "app">
       <h1>TIC TAC TOE!</h1>
       <h2>{ message }</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
-      
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
--- src/helpers.js
+++ src/helpers.js
@@ -11,7 +11,7 @@ export function calculateWinner(squares) {
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      console.log(squares[a],squares[b],squares[c]);
+    //   console.log(squares[a],squares[b],squares[c]);
       if (squares[a]!=null && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }

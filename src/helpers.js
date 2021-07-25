fa3e3b2b09f6a02404bfba1feb6597f1068a8e00
deepamasnani <asnani.deepam@gmail.com>
2021-07-26 01:35:41 +0530

    transform state to game history

--- src/helpers.js
+++ src/helpers.js
@@ -11,7 +11,8 @@ export function calculateWinner(squares) {
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      console.log(squares[a],squares[b],squares[c]);
+      if (squares[a]!=null && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
     }
